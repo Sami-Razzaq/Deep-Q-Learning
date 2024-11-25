1d0007b584f6d0a0234b73af66d799d4ee841a5e
Sami Razzaq <samirazzaq@outlook.com>
2024-11-25 11:59:52 +0500

    Added Memory & Action choosing

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 
 from pdb import set_trace
 
+# Building Q-Network
 class DeepQNetwork(object):
    def __init__(self, lr, n_actions, name, fc1_dims=256,
                input_dims=(210, 160, 4), chkpt_dir='tmp/dqn'):
@@ -68,3 +69,50 @@ class DeepQNetwork(object):
    def save_checkpoint(self):
       print('...saving checkpoint...')
       self.saver.save(self.sess, self.checkpoint_file)
+  
+# Building Agent Network to save & take actions
+class Agent(object):
+   def __init__(self, alpha, gamma, mem_size, n_actions, epsilon, 
+                batch_size, replace_target=5000, input_dims=(210,160,4),
+                q_next='tmp/q_next', q_eval='tmp/q_eval'):
+      self.n_actions = n_actions
+      self.action_space = [i for i in range(self.n_actions)]
+      self.gamma = gamma # Discount Factor
+      self.mem_size = mem_size
+      self.epsilon = epsilon
+      self.batch_size = batch_size
+      self.mem_cntr = 0
+      self.replace_target = replace_target
+      self.q_next = DeepQNetwork(alpha, n_actions, input_dims=input_dims, 
+                                 name='q_next', chkpt_dir='q_next_dir')
+      self.q_eval = DeepQNetwork(alpha, n_actions, input_dims=input_dims, 
+                                 name='q_eval', chkpt_dir='q_eval_dir')
+      
+      self.state_memory = np.zeros((self.mem_size, *input_dims))
+      self.new_state_memory = np.zeros((self.mem_size, *input_dims))
+      self.action_memory = np.zeros((self.mem_size, self.n_actions), dtype=np.int8)
+      self.reward_memory = np.zeros(self.mem_size)
+      self.terminal_memory = np.zeros(self.mem_size, dtype=np.int8)
+   
+   # Memory saving
+   def store_transition(self, state, action, reward, state_, terminal):
+      index = self.mem_cntr % self.mem_size
+      self.state_memory[index] = state
+      actions = np.zeros(self.n_actions)
+      actions[action] = 1.0
+      self.action_memory[index] = actions 
+      self.reward_memory[index] = reward 
+      self.new_state_memory[index] = state_ 
+      self.terminal_memory[index] = terminal 
+      
+      self.mem_cntr += 1
+
+   def choose_action(self, state):
+      rand = np.random()
+      if rand < self.epsilon:
+         action = np.random.choice(self.action_space)
+      else:
+         actions = self.q_eval_sess.run(self.q_eval.Q_values,
+                                        feed_dict={self.q_eval.input:state})
+         action = np.argmax(actions)
+      return action
