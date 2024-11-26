292315219b802d0656d2ff3e3d33010a9861dd04
Sami Razzaq <samirazzaq@outlook.com>
2024-11-26 13:38:59 +0500

    Code Complete

--- main_dqn.py
+++ main_dqn.py
@@ -120,3 +120,94 @@ def plot_duration(show_result=False):
       plt.plot(means.numpy)
    
    plt.pause(0.001)
+   
+def optimize_model():
+   
+   # check to ensure enough samples for mini-batch
+   if len(memory) < batch_size:
+      return
+   
+   # extract mini-batch from Replay Memory
+   # Converts batch-arrays of transitions to transitions of batch-arrays
+   transition = memory.sample(batch_size)     
+   batch = Transition(*zip(*transition))
+   
+   # Computes mask of non final states and concatenate the batch elements
+   non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
+                                           batch.next_state)), device=device)
+   
+   non_final_next_state = torch.cat([s for s in batch.next_state if s is not None])
+   
+   state_batch = torch.cat(batch.state)
+   action_batch = torch.cat(batch.action)
+   reward_batch = torch.cat(batch.reward)
+   
+   state_action_values = policy_net(state_batch).gather(1, action_batch)
+   next_state_values = torch.zeros(batch_size, device=device)
+   
+   with torch.no_grad():
+      next_state_values[non_final_mask] = target_net(non_final_next_state).max(1)[0]
+   
+   # Calculate expected q-value
+   expected_state_action_values = (next_state_values * gamma + reward_batch)
+   
+   # Compute Huber Loss
+   criterion = nn.SmoothL1Loss()
+   loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
+   
+   # Optimize the model
+   optimizer.zero_grad()
+   loss.backward()
+   
+   # In-place clipping to 100 to prevent exploding gradient problem
+   torch.nn.utils.clip_grad_value_(policy_net.parameters(), 100)
+   optimizer.step()
+   
+if torch.cuda.is_available():
+   num_episodes = 600
+else:
+   num_episodes = 300
+   
+for i in range(num_episodes):
+   
+   # initialize environment & get it's state
+   state, info = env.reset()
+   
+   state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
+   
+   for t in count():
+      action = select_action(state)
+      observation, reward, terminated, truncated, _ = env.step(action.item())
+      
+      reward = torch.tensor([reward], device=device)
+      
+      done = terminated or truncated
+      
+      if terminated:
+         next_state = None
+         
+      else:
+         next_state = torch.tensor(observation, dtype=torch.float32, device=device).unsqueeze(0)
+      
+      # Store transition
+      memory.push(state, action, next_state, reward)
+      
+      state = next_state
+      
+      optimize_model()
+      
+      target_net_state_dict = target_net.state_dict()
+      policy_net_state_dict = policy_net.state_dict()
+      
+      for key in policy_net_state_dict:
+         target_net_state_dict[key] = policy_net_state_dict[key] * tau + target_net_state_dict[key] * (1-tau)
+         
+      if done:
+         episode_durations.append(t + 1)
+         plot_duration()
+         break
+         
+print("Complete")
+plot_duration(show_result=True)
+plt.ioff()
+plt.show()
